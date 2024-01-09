cc96a3bcb26308e9837ee57c64fcf1b2077a3db1
Juff-Ma <julianrossbach3012@gmail.com>
2024-01-09 17:13:48 +0100

    Add Window base structure and top level enums

---
+++ src/WebUI.NET/Browsers.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+//TODO: Add Comments
+
+namespace WebUI
+{
+    public enum Browsers
+    {
+        NoBrowser = 0,
+        AnyBrowser = 1,
+        Chrome = 2,
+        Firefox = 3,
+        Edge = 4,
+        Safari = 5,
+        Chromium = 6,
+        Opera = 7,
+        Brave = 8,
+        Vivaldi = 9,
+        Epic = 10,
+        Yandex = 11,
+        ChromiumBased = 12
+    }
+}
---
+++ src/WebUI.NET/Runtimes.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+//TODO: Add Comments
+
+namespace WebUI
+{
+    public enum Runtimes
+    {
+        None = 0,
+        Deno = 1,
+        NodeJS = 2
+    }
+}
