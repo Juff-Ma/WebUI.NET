d19d9fe2d8d113df7a4f280103add7f5761c5bac
Juff-Ma <julianrossbach3012@gmail.com>
2024-01-22 18:15:49 +0100

    aded inital example for events, currently does not work. need to keep alive pointer

---
+++ examples/events/Program.cs
@@ -0,0 +1,26 @@
+﻿using WebUI;
+
+Window window = new();
+window.Show("""
+<html>
+    <script src="webui.js"></script> 
+    <head>
+        <title>WebUI</title>
+    </head>
+    <body>
+        <h1>Events Test</h1>
+    </body>
+</html>
+""");
+
+var defaultHandler = window.RegisterDefaultEventHandler();
+
+defaultHandler.OnConnect += (_) => {
+    Console.WriteLine("Connected");
+};
+
+defaultHandler.OnDisconnect += (_) => {
+    Console.WriteLine("Disconnected");
+};
+
+Utils.WaitForExit();
