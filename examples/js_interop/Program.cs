269fbc4923b8227cc805fc811d86350398d3bcd8
Juff-Ma <contact@juffma.de>
2024-01-27 18:34:26 +0100

    initial js_interop

--- examples/js_interop/Program.cs
+++ examples/js_interop/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using WebUI;
+
+Window window = new();
+ulong handlerid = 0;
+handlerid = window.RegisterEventHandler((@event, _, id) =>
+{
+    if (handlerid == id)
+    {
+        var window = @event.Window;
+        window.InvokeJavaScript("""
+            var button = document.getElementById("replace");
+            var text = document.createElement("p");
+            text.innerHTML = "Successfully replaced!";
+            button.parentNode.replaceChild(text, button);
+            """);
+    }
+    return null;
+}, "replace");
+
+window.Show("index.html");
+
+Utils.WaitForExit();
