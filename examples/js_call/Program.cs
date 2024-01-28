8a52b67945a91ef46264518da0ad86163182046b
Juff-Ma <contact@juffma.de>
2024-01-28 15:16:47 +0100

    Added js_interop example

---
+++ examples/js_call/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+using WebUI;
+
+Window window = new();
+window.Show("index.html");
+
+for (int i = 1; i <= 100; i++)
+{
+    await Task.Delay(100);
+    window.InvokeJavaScript("setText", Encoding.UTF8.GetBytes($"Invoke number {i}"));
+}
+
+Utils.WaitForExit();
