34f713dbf27665c77307d743e4ff4313a5ad044d
Juff-Ma <contact@juffma.de>
2024-02-01 18:01:17 +0100

    Added c# version of text-editor example from webui upstream

---
+++ examples/text_editor/Program.cs
@@ -0,0 +1,16 @@
+﻿using TextEditor;
+using WebUI;
+
+WindowProperties windowProperties = new()
+{
+    Width = 800,
+    Height = 600,
+
+    RootFolder = "ui"
+};
+
+TextEditorWindow window = new(windowProperties);
+window.Show();
+
+Utils.WaitForExit();
+Utils.Clean();
---
+++ examples/text_editor/TextEditorWindow.cs
@@ -0,0 +1,39 @@
+﻿namespace TextEditor;
+
+using WebUI;
+using WebUI.Events;
+
+internal class TextEditorWindow : Window, IEventHandler
+{
+    private const string CloseButtonId = "__close-btn";
+    private const string MainPage = "index.html";
+
+    private readonly ulong _handleId;
+
+    public object? HandleEvent(Event @event, string element, ulong handlerId)
+    {
+        if (element == CloseButtonId && handlerId == _handleId)
+        {
+            Close();
+        }
+
+        // if you close the Window or invalidate it in any other way during an Event you *MUST* return null
+        // otherwise buggy behavior will occur and the program will probably crash
+        return null;
+    }
+
+    public TextEditorWindow(WindowProperties properties) : base(properties)
+    {
+        // normally you would probably register the Event handler of the window itself for all events, but we only have one event
+        _handleId = RegisterEventHandler(this, CloseButtonId);
+    }
+
+    public bool Show()
+    {
+        if (!Show(MainPage, Browser.ChromiumBased))
+        {
+            return Show(MainPage);
+        }
+        return true;
+    }
+}
