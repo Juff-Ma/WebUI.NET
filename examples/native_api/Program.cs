ab616b89875b5f9433bf289237689b9aaceb713c
Juff-Ma <contact@juffma.de>
2024-02-02 18:46:58 +0100

    Added example of using Native OS apis with the browser window - Show how to retrieve the browsers Process and Window handle - Show how to attach a Windows Message box to the Window using this technique

---
+++ examples/native_api/Program.cs
@@ -0,0 +1,34 @@
+﻿// Ignore Spelling: Api
+
+using System.Runtime.InteropServices;
+using WebUI;
+
+Window window = new();
+
+ulong handleId = 0;
+
+handleId = window.RegisterEventHandler((@event, _, handlerId) =>
+{
+    if (handleId == handlerId)
+    {
+        var browser = @event.Window.GetBrowserChildProcess();
+        NativeApi.Natives.MessageBox(browser.MainWindowHandle,
+            "This is a message box attached to the Browser window",
+            "Message", 0);
+    }
+
+    return null;
+}, "show");
+
+window.Show("index.html");
+
+Utils.WaitForExit();
+
+namespace NativeApi
+{
+    internal static partial class Natives
+    {
+        [LibraryImport("User32.dll", EntryPoint = "MessageBoxW", StringMarshalling = StringMarshalling.Utf16)]
+        public static partial int MessageBox(nint hWnd, string lpText, string lpCaption, uint uType);
+    }
+}
